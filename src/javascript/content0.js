16ea5eb4b5dacb6c671e82637d1bbaa4f6296c1b
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-17 00:55:26 +0900

    section0 done

---
+++ src/javascript/content0.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sogo on 17/09/2017.
+ */
+
+$(document).ready(function(){
+    if (navigator.userAgent.match(/(iPod|iPhone)/)) {
+        // 에이전트가 아이폰이면 리소스를 넣지 않습니다
+    }
+    else {
+        // 에이전트가 아이폰이 아닐 경우
+        $('#video-wrapper').find('video').append('<source src="./movie/tank100-01.mp4" type="video/mp4">');
+        $('#video-wrapper').find('video')[0].load();
+    }
+});
