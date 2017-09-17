99098a1d08a1084c4929d147fc88466d62ea0701
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-17 16:53:09 +0900

    c3 done

--- src/javascript/content3.js
+++ src/javascript/content3.js
@@ -2,11 +2,16 @@
  * Created by sogo on 17/09/2017.
  */
 
+$(document).ready(function(){
+    $('#c3-background').fadeOut();
+});
+
 var is_working = false;
 
 function flewAttributes(value) {
     if (value == 3 && is_working == false) {
         is_working = true;
+        $('#c3-background').fadeIn();
         $('#c3-slide1').animate({left: "0%", bottom: "3%"}, 800, function () {});
         $('#c3-slide2').animate({left: "0%", bottom: "5%"}, 1000, function () {});
         $('#c3-slide3').animate({left: "0%", bottom: "5%"}, 700, function () {});
@@ -14,6 +19,7 @@ function flewAttributes(value) {
     }
     if (value != 3 && is_working == true) {
         is_working = false;
+        $('#c3-background').fadeOut();
         $('#c3-slide1').animate({left: "0", bottom: "160%"}, 400, function () {});
         $('#c3-slide2').animate({left: "-400%", bottom: "8%"}, 500, function () {});
         $('#c3-slide3').animate({left: "-600%", bottom: "7%"}, 350, function () {});
