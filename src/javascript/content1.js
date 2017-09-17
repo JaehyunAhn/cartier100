0df5018d903fc070c9fcda0d152ac6bbac888090
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-17 16:59:47 +0900

    c1 done

--- src/javascript/content1.js
+++ src/javascript/content1.js
@@ -1,6 +1,12 @@
 /**
  * Created by sogo on 17/09/2017.
  */
+// 100 주념 시계 설명 바뀜
+var watch_object = {};
+watch_object['Brancard'] = 'AAAA';
+watch_object['Roman Numerals'] = 'BBB';
+watch_object['Sapphire Cabochon'] = 'CCC';
+watch_object['Chemin De Fer'] = 'DDDD';
 
 $(document).ready(function() {
     $(".c1-button").hover(function(){
@@ -8,6 +14,7 @@ $(document).ready(function() {
         if (!$(this).hasClass('animated')) {
             // text replace xxx to +
             $(this).text($(this)[0].attributes['name'].value);
+            $('#c1-bottom').text(watch_object[$(this)[0].attributes['name'].value]);
             $(this).dequeue().stop().animate({ width: "20%" }, 250, function() {
             });
         }
