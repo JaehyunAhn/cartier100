ef28373c4bf4a1c4d5efc8c02fd37d306ba988c0
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-18 01:51:28 +0900

    section 6 done

--- src/javascript/content3.js
+++ src/javascript/content3.js
@@ -6,19 +6,19 @@ $(document).ready(function(){
     $('#c3-background').fadeOut();
 });
 
-var is_working = false;
+var is_working_for3 = false;
 
 function flewAttributes(value) {
-    if (value == 3 && is_working == false) {
-        is_working = true;
+    if (value == 3 && is_working_for3 == false) {
+        is_working_for3 = true;
         $('#c3-background').fadeIn();
         $('#c3-slide1').animate({left: "0%", bottom: "3%"}, 800, function () {});
         $('#c3-slide2').animate({left: "0%", bottom: "5%"}, 1000, function () {});
         $('#c3-slide3').animate({left: "0%", bottom: "5%"}, 700, function () {});
         $('#c3-slide4').animate({left: "0%", bottom: "0"}, 500, function() {});
     }
-    if (value != 3 && is_working == true) {
-        is_working = false;
+    if (value != 3 && is_working_for3 == true) {
+        is_working_for3 = false;
         $('#c3-background').fadeOut();
         $('#c3-slide1').animate({left: "0", bottom: "160%"}, 400, function () {});
         $('#c3-slide2').animate({left: "-400%", bottom: "8%"}, 500, function () {});
--- src/javascript/content6.js
+++ src/javascript/content6.js
@@ -1,3 +1,15 @@
-/**
- * Created by sogo on 17/09/2017.
- */
+/* Created by sogo on 17/09/2017.*/
+
+$(document).ready(function(){
+    $('#c6-coverflow').flipster({
+        fadeIn: 300,
+        style: 'carousel',
+        spacing: -0.5,
+        buttons: true,
+        start:0,
+        enableNavButtons: true,
+        onItemSwitch: function(currentItem, previousItem) {
+            console.log(currentItem);
+        }
+    });
+});
