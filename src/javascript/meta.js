904f9d5639bc2c2d9027496b1160eea2abc48922
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-23 07:00:38 +0900

    minor bug correction

--- src/javascript/meta.js
+++ src/javascript/meta.js
@@ -9,11 +9,11 @@ $(window).on("load", function(){
         var item = $('#' + $(this)[0].attributes['meta'].value)
         if (item.is(':visible')){
             item.css('display', 'none');
-            $("#page-container, #footer, #top").toggleClass('blur-effect');
+            $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
         }
         else{
             item.css('display', 'block');
-            $("#page-container, #footer, #top").toggleClass('blur-effect');
+            $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
         }
 
     });
