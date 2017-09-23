27270c36bfcf2e061b0e670e6523615c6737fd69
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-24 03:04:18 +0900

    mobile, desktop following saparation

--- src/javascript/content2.js
+++ src/javascript/content2.js
@@ -5,5 +5,5 @@ $(window).scroll(function() {
     // 현재 스크롤 값과 C2 review wrapper 값을 가지고 포지션을 구한다
     var c2_review_wrapper = $('#c2-review-wrapper');
     var c2_bg_img_pos = $(window).scrollTop() - c2_review_wrapper.offset().top;
-    c2_review_wrapper.css('background-position', 'left ' + ((c2_bg_img_pos)) + 'px');
+    c2_review_wrapper.css('background-position-y', ((c2_bg_img_pos)) + 'px');
 });
