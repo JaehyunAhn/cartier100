315380d22fced72239a961dcea97a3a8d03ee50a
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-24 12:03:03 +0900

    }

--- src/javascript/meta.js
+++ src/javascript/meta.js
@@ -3,18 +3,33 @@
  */
 $(window).on("load", function(){
     // trigger 가 move top 하는 걸 방지
-    $('.trigger').click(function(e) {
-        e.preventDefault();
-        // show 대응하는 toggle class
-        var item = $('#' + $(this)[0].attributes['meta'].value)
-        if (item.is(':visible')){
-            item.css('display', 'none');
-            $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
-        }
-        else{
-            item.css('display', 'block');
-            $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
-        }
-
-    });
+    //$('.trigger').click(function(e) {
+    //    e.preventDefault();
+    //});
+    //    // show 대응하는 toggle class
+    //    var item = $('#' + $(this)[0].attributes['meta'].value)
+    //    if (item.is(':visible')){
+    //        item.css('display', 'none');
+    //        $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
+    //    }
+    //    else{
+    //        item.css('display', 'block');
+    //        $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
+    //    }
+    //
+    //});
 });
+
+function toggleClicked(element) {
+    // show 대응하는 toggle class
+    console.log($(element));
+    var item = $('#' + $(element)[0].attributes['meta'].value)
+    if (item.is(':visible')){
+        item.css('display', 'none');
+        $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
+    }
+    else{
+        item.css('display', 'block');
+        $("#page-container, #footer, #top, #menu").toggleClass('blur-effect');
+    }
+};
