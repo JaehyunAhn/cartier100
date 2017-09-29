e94a1f55e5f94805d74991799839a763631ba3b8
Jaehyun Ahn <sogosonnet@gmail.com>
2017-09-30 00:16:45 +0900

    x button inserted

--- src/javascript/content6.js
+++ src/javascript/content6.js
@@ -1,19 +1,5 @@
 /* Created by sogo on 17/09/2017.*/
 
 $(window).on("load", function(){
-    $('#c6-coverflow').flipster({
-        fadeIn: 300,
-        style: 'carousel',
-        spacing: -0.5,
-        buttons: true,
-        click: true,
-        scrollwheel: false,
-        keyboard: false,
-        touch: true,
-        start:0,
-        enableNavButtons: true,
-        onItemSwitch: function(currentItem, previousItem) {
-            //console.log(currentItem);
-        }
-    });
+
 });
